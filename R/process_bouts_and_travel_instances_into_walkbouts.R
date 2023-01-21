29b45923cb85e10f99944fed48e265c0e4bbd7d4
Lauren B Wilner <wilner.lauren@gmail.com>
2023-01-21 13:21:16 -0800

    Add skeletons for public functions

---
+++ R/process_bouts_and_travel_instances_into_walkbouts.R
@@ -0,0 +1,6 @@
+process_bouts_and_travel_instances_into_walkbouts <- function(bouts, travel_instances){
+  # TODO:
+  #  - Define output schema and write some validation
+  #  - Write processing code
+  print('processing bouts and travel instances')
+}
---
+++ R/process_gps_data_into_travel_instances.R
@@ -0,0 +1,10 @@
+process_gps_data_into_travel_instances <- function(gps_data) {
+  # TODO:
+  #  - Define input schema and validate inputs match.
+  #   - What are the column names
+  #   - What are the data types of the columns
+  #   - Are null values allowed in each column and how are they represented.
+  #  - Define the output schema and write some validation
+  #  - Write the processing code in the middle
+  print('processing gps data into travel instances')
+}
